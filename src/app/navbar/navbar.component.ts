a27e0855c69ffd71b82696dc0e66c1b5e8571fb7
Shwe N Aye <shweaye810@gmail.com>
2019-06-26 15:08:19 -0500

    add search(), map search html ngModel
    - give journalService searchtext
    - reload home page
    - change text to logo image
    - clean up

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { JournalService } from '../journal.service';
+import { AuthService } from '../auth.service';
+import { HomeComponent } from '../home/home.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +10,20 @@ import { JournalService } from '../journal.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  searchText: string = "";
+  @Output() searchedText =  new EventEmitter<string>();
 
-  constructor(private journalService: JournalService) { }
+  constructor(private journalService: JournalService,
+    private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+  search() {
+    console.log("you pressed enter. searchText:", this.searchText)
+
+    this.journalService.setSearchText(this.searchText);
+    this.router.navigate(["/home"])
+
   }
 
 }
