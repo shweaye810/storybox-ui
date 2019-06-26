1367b036adee8e86187411d4cb4f7b7bb5f0f944
Shwe N Aye <shweaye810@gmail.com>
2019-06-26 15:17:53 -0500

    impl addJournal and setSearchText
    - call respective endpoints
    - getJournal return value based on searchText

--- src/app/journal.service.ts
+++ src/app/journal.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
+import { catchError, map, tap } from 'rxjs/operators';
 import { Journal } from './journal';
-import { JOURNALS } from './mock-journals';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class JournalService {
+  private journalURL: string = environment.journalURL;
+  private searchText: string;
+  constructor(private http: HttpClient) { }
+  getJournals(): Observable<any> {
+    console.log("JournalService>getJournals>searchText", this.searchText)
+    if (this.searchText)
+      return this.http.get<any[]>(this.journalURL + "/search", 
+      {params: {"text" : this.searchText} } );
+    return this.http.get<any[]>(this.journalURL)
+  }
+  addJournal(journal: Journal): Observable<Journal> {
+    return this.http.post<any>(this.journalURL, journal, httpOptions);
+  }
 
-  constructor() { }
-
-  getJournals(): Observable<Journal[]> {
-    return of(JOURNALS)
+  setSearchText(text: string) {
+    this.searchText = text;
   }
 }
