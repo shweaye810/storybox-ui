52380a1ea3748ccc882747df9718007f8b1059d0
Shwe N Aye <shweaye810@gmail.com>
2019-06-26 15:14:08 -0500

    add post button, show tag, impl auto reload data
    - make journal box to autoresize
    - post button will append newly returned data from service
    - subscribe to router navigate, if landed on home, reload data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Journal } from 'src/app/journal';
 import { DatePipe } from '@angular/common';
 import { JournalService } from '../journal.service';
+import { AuthService } from '../auth.service';
+import { stringify } from '@angular/core/src/util';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    this.navSub.unsubscribe();
+  }
 
   journals: Journal[] = [];
-  name: String = "Shwe Aye";
-  constructor(private datePipe: DatePipe, private journalService: JournalService) { }
+  token: any;
+  searchText: string;
+  navSub: Subscription;
+
+  constructor(private datePipe: DatePipe, private journalService: JournalService,
+    private authService: AuthService, private router: Router) {
+    this.navSub = this.router.events.subscribe((e: any) => {
+      if (e instanceof NavigationEnd) {
+        this.getJournals();
+      }
+
+    });
+  }
 
   ngOnInit() {
+
+    console.log("ngOnInit>this.journals: ", this.journals)
     this.getJournals();
+    console.log("HomeComponent>token", this.authService.token);
   }
 
   getJournals() {
-    this.journalService.getJournals().subscribe(journals => this.journals = journals);
+    let obs = null;
+
+    obs = this.journalService.getJournals()
+    obs.subscribe((journals: any) => {
+      console.log("HomeComponent>getJournals", journals);
+      this.journals = journals
+      console.log("HomeComponent>this.journals", this.journals);
+
+    });
+  }
+
+  add(text: String): void {
+    console.log("text area NgModel text: ", text);
+    text = text.trim();
+    this.journalService.addJournal({ text } as Journal).subscribe(journal => {
+      this.journals.shift();
+      this.journals.unshift(journal)
+    });
   }
 
 }
